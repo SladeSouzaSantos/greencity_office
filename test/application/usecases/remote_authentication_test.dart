6e890235168c84fd216196a815467eb5e7e8546c
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2023-02-19 17:38:51 -0300

    Teste - RemoteAuthentication deve chamar UnexpectedError quando ClientGeneric retornar 500.

--- test/application/usecases/remote_authentication_test.dart
+++ test/application/usecases/remote_authentication_test.dart
@@ -45,4 +45,11 @@ void main(){
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test("Deve chamar UnexpectedError quando ClientGeneric retornar 500.", () async{
+    when(clientGeneric!.request(url: anyNamed("url"), method: anyNamed("method"), body: anyNamed("body"))).thenThrow(HttpError.serverError);
+    final future = sut!.auth!(params: params!);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
