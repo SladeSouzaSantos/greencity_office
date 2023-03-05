5f03c1dc7e7c4f67659b2e73ed08cc10b57389d4
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2023-03-05 17:43:37 -0300

    Criação do LoginPage

---
+++ lib/main/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../ui/components/components.dart';
+import 'config.dart';
+
+
+
+void main() async{
+  await initConfiguration();
+
+  runApp(const App());
+}
