697c5ac0ce125dcd0f69a5fbb3bfc89649e96d9a
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2023-03-06 10:07:33 -0300

    Estilização do LoginPage e criação dos componentes LoginHead e Headline1.

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../pages/pages.dart';
 
@@ -7,10 +8,55 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
+    const primaryColor = Color.fromRGBO(13, 76, 128, 1);
+    const primaryColorDarkScheme = Color.fromRGBO(159, 202, 255, 1);
+    const primaryColorLightScheme = Color.fromRGBO(8, 97, 164, 1);
+    const backgroundColorLightScheme = Color.fromRGBO(255, 251, 255, 1);
+
+    return MaterialApp(
       title: "Greencity Office",
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: primaryColor,
+        primaryColorLight: primaryColorLightScheme,
+        primaryColorDark: primaryColorDarkScheme,
+        backgroundColor: backgroundColorLightScheme,
+
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: primaryColorLightScheme
+          ),
+        ),
+
+        inputDecorationTheme: const InputDecorationTheme(
+          enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: primaryColorLightScheme)
+          ),
+          focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: primaryColor)
+          ),
+          alignLabelWithHint: true
+        ),
+
+        buttonTheme: ButtonThemeData(
+          colorScheme: const ColorScheme.light(primary: primaryColor),
+          buttonColor: primaryColor,
+          splashColor: primaryColorLightScheme,
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          textTheme: ButtonTextTheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          )
+        ),
+
+      ),
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: const LoginPage(),
     );
   }
 }
